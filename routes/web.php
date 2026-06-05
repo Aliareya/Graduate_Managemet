a38177fc83dadeba518b30c5d6c41f2a1ad17d1c
Alireza <aliareya2003@gmail.com>
2026-06-05 23:43:49 +0430

    FFFF

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.departments.Index');
+    return view('admin.pages.dashboard.index');
 })->name('home');
 
 
